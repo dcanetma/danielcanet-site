bce6061d89990951181ff3974d9c4d3c837d3530
Daniel Canet <dcanet@wiris.com>
2021-03-03 23:03:18 +0100

    Remove jQuery dependency

--- themes/danielcanet/source/assets/js/app.js
+++ themes/danielcanet/source/assets/js/app.js
@@ -1,6 +1,7 @@
-$(function () {
-  $("a.toggle-mode").on("click", function (event) {
-    event.preventDefault();
-    $("body").toggleClass("dark-mode");
-  });
-});
+
+// Select the button
+const btn = document.querySelector('.toggle-mode');
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');  
+})
